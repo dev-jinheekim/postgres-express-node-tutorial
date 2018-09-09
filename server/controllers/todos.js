6fc4fd92359c4f09f50a286c6dd65c8880e22d67
jay <mail@kimjinhee.me>
2018-09-10 02:58:35 +0900

    Todos 만들기 - 컨트롤러와 라우팅 제작

---
+++ server/controllers/todos.js
@@ -0,0 +1,12 @@
+const Todos = require('../models').Todo;
+
+module.exports = {
+    create(req, res){
+        return Todos
+            .create({
+                title: req.body.title,
+            })
+            .then(todo => res.status(201).send(todo))
+            .catch(error => res.status(400).send(error));
+    },
+};
