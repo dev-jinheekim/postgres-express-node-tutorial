2c8053b869f2804ce59ec3158527b1ca21fa0b30
jay <mail@kimjinhee.me>
2018-09-10 05:13:50 +0900

    할일 삭제

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -62,4 +62,20 @@ module.exports = {
             })
             .catch((error) => res.status(400).send(error));
     },
+    destroy(req, res) {
+        return Todos
+            .findById(req.params.todoId)
+            .then(todo => {
+                if (!todo) {
+                    return res.status(400).send({
+                        message: 'Todo Not Found',
+                    });
+                }
+                return todo
+                    .destroy()
+                    .then(() => res.status(204).send({ message: 'No Content' }))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
+    },
 };
